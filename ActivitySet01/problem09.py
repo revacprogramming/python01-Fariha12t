ff3b98a053c5c4fbb3896f10e2bd22887ab831d0
Fariha12t <fariha.taneem@gmail.com>
2022-06-03 19:30:56 +0530

    done

--- ActivitySet01/problem09.py
+++ ActivitySet01/problem09.py
@@ -1,3 +1,14 @@
 # Lists
 
-filename = "dataset/romeo.txt"
+filename = "dataset,romeo.txt"
+fh = open(filename)
+stuff= list()
+
+for line in fh:
+    line = line.rstrip()
+    words = line.split()
+    for w in words:
+        if w not in stuff:
+            stuff.append(w)
+stuff.sort()
+print(stuff)
