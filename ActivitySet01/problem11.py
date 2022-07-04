62f995eaa76f229cace47f714ec1670dcc17b3b9
Fariha12t <fariha.taneem@gmail.com>
2022-07-04 19:21:50 +0530

    11

--- ActivitySet01/problem11.py
+++ ActivitySet01/problem11.py
@@ -1,3 +1,14 @@
 # Tuples
 
 filename = "dataset/mbox-short.txt"
+handle = open(filename)
+d1 = dict()
+for line in handle:
+    if line.startswith("From "): 
+        lst = line.split()
+        num = lst[5]
+        time = num.split(":")
+        d1[time[0]] = d1.get(time[0],0) + 1
+ftime = sorted(d1.items())
+for (k,v) in ftime:
+    print(k,v)
