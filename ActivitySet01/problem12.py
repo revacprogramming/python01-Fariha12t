378f5cc2b415f2d537e6df7cbf4f4b7f8d4a84c8
Fariha12t <fariha.taneem@gmail.com>
2022-07-17 11:29:33 +0530

    done

--- ActivitySet01/problem12.py
+++ ActivitySet01/problem12.py
@@ -1,2 +1,9 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
+# Search for lines that start with From and have an at sign
+import re
+hand = open('dataset/mbox-short.txt')
+for line in hand:
+    line = line.rstrip()
+    if re.search('^From:.+@', line):
+        print(line)
